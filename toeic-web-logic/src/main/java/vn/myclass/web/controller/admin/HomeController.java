a22fb94d00daaf0cefd3723c9f8038ee183529ff
ManNgo2016 <nguyenngocman2016@gmail.com>
2018-07-01 18:52:34 +0700

    Add decorator 1

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/controller/admin/HomeController.java
@@ -0,0 +1,7 @@
+package vn.myclass.web.controller.admin;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/admin-home.html")
+public class HomeController {
+}
---
+++ toeic-web-logic/src/main/java/vn/myclass/web/controller/web/HomeController.java
@@ -0,0 +1,7 @@
+package vn.myclass.web.controller.web;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/home.html")
+public class HomeController {
+}
